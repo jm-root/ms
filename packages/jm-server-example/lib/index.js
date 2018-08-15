19becd44ba4db6836f39b1859d5b1c3e91c9751f
jamma <jeff@jamma.cn>
2018-08-15 15:59:06 +0800

    wrapper

--- packages/jm-server-example/lib/index.js
+++ packages/jm-server-example/lib/index.js
@@ -1,8 +1,11 @@
+const error = require('jm-err')
+const wrapper = require('jm-ms-wrapper')
 const MS = require('jm-ms')
 const ms = new MS()
 
 let $ = {
   router () {
+    const wrap = wrapper()
     const router = ms.router()
     router
       .use(opts => {
@@ -10,6 +13,12 @@ let $ = {
       })
       .add('/', opts => {
         return opts
+      })
+      .add('/err', wrap(opts => {
+        throw error.err(error.Err.FAIL)
+      }))
+      .add('/err2', opts => { // no wrap
+        throw error.err(error.Err.FAIL)
       })
     return router
   }
