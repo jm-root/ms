8a5603b78baa952075a42e6c2459e4575fe4ccce
jamma <jeff@jamma.cn>
2018-08-22 11:09:24 +0800

    browser

--- packages/jm-ms/History.md
+++ packages/jm-ms/History.md
@@ -1,4 +1,7 @@
 # History
 
+## 2.0.2 / 2018-08-22
+- fixed: browser
+
 ## 2.0.0 / 2018-07-19
 - create
--- packages/jm-ms/lib/browser.js
+++ packages/jm-ms/lib/browser.js
@@ -4,8 +4,8 @@ class $ extends MS {
   constructor (opts) {
     super(opts)
     this
-      .use(require('jm-ms-http-client'))
-      .use(require('jm-ms-ws-client'))
+      .use(require('jm-ms-http-client/lib/browser'))
+      .use(require('jm-ms-ws-client/lib/browser'))
   }
 }
 
